877030c1ccdd5c0e6b444d7a49ba4fd65a33489a
inamarketin <ina279@gmail.com>
2017-03-01 18:18:11 +0100

    Bok
    Bok

---
+++ js/ina.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+
+$("#submit-btn").click(function(){
+
+var email = $("#input-email").val()
+var password = $("#input-password").val()
+
+console.log(email)
+console.log(password)
+
+
+
+})
+
+})
+	
